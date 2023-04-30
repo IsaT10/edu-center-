733a64a1d03ef670ecc8cbe14711475b22bb58ff
IsaT <rakiib010@gmail.com>
2023-04-30 12:04:19 +0600

    first commit

---
+++ src/config/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyALVFFmOMcxaFaa2VAt-QxDGnUFBHRQAo4",
+  authDomain: "edu-center-5d598.firebaseapp.com",
+  projectId: "edu-center-5d598",
+  storageBucket: "edu-center-5d598.appspot.com",
+  messagingSenderId: "777018694472",
+  appId: "1:777018694472:web:1428a19a44557db17cf17d",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#f94a93",
+
+          secondary: "#630cce",
+
+          accent: "#00a4d6",
+
+          neutral: "#1B1D32",
+
+          "base-100": "#F5F4F5",
+
+          info: "#3251CD",
+
+          success: "#51ECCA",
+
+          warning: "#ECAA36",
+
+          error: "#E95D7E",
+        },
+      },
+    ],
+  },
+  plugins: [require("daisyui")],
+};
